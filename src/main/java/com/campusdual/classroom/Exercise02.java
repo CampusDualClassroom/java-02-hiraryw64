220de124d4f6f6b0b6c7f844f35dc1585f1ae642
hiraryw64 <hiraryw64@gmail.com>
2024-09-25 11:39:52 +0200

    Ejercicio resuelto

--- src/main/java/com/campusdual/classroom/Exercise02.java
+++ src/main/java/com/campusdual/classroom/Exercise02.java
@@ -7,12 +7,12 @@ public class Exercise02 {
         double a, l;
 
         // TODO: ↓ Fórmula del área de un círculoS: a = radio * radio * Número PI
-
+        a = r * r * Math.PI;
         System.out.println("Area: " + a);
 
         // TODO: ↓ Fórmula de la longitud de una circunferencia: l = 2 * radio * Número PI
-
-        System.out.println("Longitud:  " + l);
+        l = 2 * r * Math.PI;
+        System.out.println("Longitud: " + l);
 
     }
 }
